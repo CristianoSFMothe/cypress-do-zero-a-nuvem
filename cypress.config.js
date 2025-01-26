a15db35777afefa47ebe326bc936a1e80afd730e
CristianoSFMothe <cristianodevsystem@gmail.com>
2025-01-26 17:04:44 -0300

    feat: using lodash  and invoke and request

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: process.env.CYPRESS_projectId,
+  projectId: "qywha9",
   viewportHeight: 880,
   viewportWidth: 1280,
   e2e: {}
